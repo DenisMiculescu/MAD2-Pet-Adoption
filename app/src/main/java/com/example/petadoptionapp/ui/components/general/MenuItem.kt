91dc9a0b97fedca72b79745a8bb1674cc32de3a5
denis miculescu <20098078@mail.wit.ie>
2025-03-05 16:39:18 +0000

    Switching between screens implemented

---
+++ app/src/main/java/com/example/petadoptionapp/ui/components/general/MenuItem.kt
@@ -0,0 +1,5 @@
+package com.example.petadoptionapp.ui.components.general
+
+enum class MenuItem {
+    Adopt, Report
+}
--- app/src/main/java/com/example/petadoptionapp/ui/screens/ScreenReport.kt
+++ app/src/main/java/com/example/petadoptionapp/ui/screens/ScreenReport.kt
@@ -1,15 +1,24 @@
 package com.example.petadoptionapp.ui.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.petadoptionapp.R
 import com.example.petadoptionapp.data.AdoptionModel
 import com.example.petadoptionapp.data.fakeAdoptions
+import com.example.petadoptionapp.ui.components.general.Centre
 import com.example.petadoptionapp.ui.components.report.AdoptionCardList
 import com.example.petadoptionapp.ui.components.report.ReportText
 import com.example.petadoptionapp.ui.theme.PetAdoptionAppTheme
@@ -28,9 +37,20 @@ fun ScreenReport(modifier: Modifier = Modifier,
             ),
         ) {
             ReportText()
-            AdoptionCardList(
-                adoptions = adoptions
-            )
+            if(adoptions.isEmpty())
+                Centre(Modifier.fillMaxSize()) {
+                    Text(color = MaterialTheme.colorScheme.secondary,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 30.sp,
+                        lineHeight = 34.sp,
+                        textAlign = TextAlign.Center,
+                        text = stringResource(R.string.empty_list)
+                    )
+                }
+            else
+                AdoptionCardList(
+                    adoptions = adoptions
+                )
         }
     }
 }
